1ae6a7134591b840e6d58b62e61c035fd665960a
Arpan <arpan@Arpans-MBP.lan>
2026-05-30 09:11:08 -0400

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,27 @@
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Carolina Workflow",
   description:
     "Helping Carolina businesses spend less time on administrative work.",
+  openGraph: {
+    title: "Carolina Workflow",
+    description:
+      "Helping Carolina businesses spend less time on administrative work.",
+    url: "https://carolinaworkflow.com",
+    siteName: "Carolina Workflow",
+  },
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
